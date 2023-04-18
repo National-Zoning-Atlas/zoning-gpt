48f35c29afa4fc873ea7f3fee842436b9155836a
Max Dumas <max@dumas.nyc>
2023-04-18 16:57:21 -0400

    Use stuffing approach to generate better results

--- zoning/prompting/extract.py
+++ zoning/prompting/extract.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import json
 from pathlib import Path
 from typing import Generator
@@ -5,7 +6,7 @@ from typing import Generator
 from minichain import OpenAI, prompt
 from pydantic import BaseModel
 
-from ..utils import get_project_root, load_jsonl
+from ..utils import get_project_root, load_jsonl, chunks
 from .search import nearest_pages, PageSearchOutput
 
 with Path(__file__).parent.joinpath("thesaurus.json").open(encoding="utf-8") as f:
@@ -15,61 +16,88 @@ extraction_tmpl = str(
     (get_project_root() / "templates" / "extraction.pmpt.tpl").relative_to(Path.cwd())
 )
 
+
 class District(BaseModel):
     T: str
     Z: str
+
 
 class PromptOutput(BaseModel):
     answer: str
     extracted_text: str
     pages: list[int]
 
+
 class LookupOutput(BaseModel):
-    answer: PromptOutput | None = None # type: ignore
+    output: PromptOutput | None = None  # type: ignore
     search_pages: list[PageSearchOutput] = []
     """
     The set of pages, in descending order or relevance, used to produce the
     result.
     """
 
+
 class AllLookupOutput(BaseModel):
     town: str
     district: District
     sizes: dict[str, LookupOutput]
 
-@prompt(OpenAI(model="gpt3.5-turbo"), template_file=extraction_tmpl, parser="json")
+
+@prompt(OpenAI(model="gpt4"), template_file=extraction_tmpl, parser="json")
 def lookup_term_prompt(model, page_text, district, term) -> PromptOutput:
-    return model(dict(
-        passage=page_text,
-        term=term,
-        synonyms=" ,".join(thesaurus.get(term, [])),
-        zone_name=district["T"],
-        zone_abbreviation=district["Z"],
-    ))
-
-def extract_size(town, district, term, top_k_pages) -> LookupOutput | None:
-    pages = nearest_pages(town, district, term)[:top_k_pages]
-    top_page = next(iter(pages), None)
-
-    if not top_page:
-        return LookupOutput()
-
-    return LookupOutput(
-        answer=lookup_term_prompt(top_page.text, district, term).run(), # type: ignore
-        search_pages=pages,
+    return model(
+        dict(
+            passage=page_text,
+            term=term,
+            synonyms=" ,".join(thesaurus.get(term, [])),
+            zone_name=district["T"],
+            zone_abbreviation=district["Z"],
+        )
     )
 
-def extract_all_sizes(town_districts: list[dict], terms: list[str], top_k_pages: int) -> Generator[AllLookupOutput, None, None]:
+
+def extract_size(town, district, term, top_k_pages) -> LookupOutput:
+    pages = nearest_pages(town, district, term)[:top_k_pages]
+
+    if len(pages) == 0:
+        return LookupOutput()
+
+    # Stuff all pages into prompt, in order of page number
+    all_page = reduce(
+        lambda a, b: a + b.text, sorted(pages, key=lambda p: p.page_number), ""
+    )
+
+    # This is the length of the prompt before any template interpolation
+    # TODO: Determine this automatically
+    prompt_base_token_length = 256
+    for chunk in chunks(all_page, 2047 - prompt_base_token_length):
+        result: PromptOutput | None = lookup_term_prompt(chunk, district, term).run()  # type: ignore
+        if result is not None:
+            return LookupOutput(
+                output=result,
+                search_pages=pages,
+            )
+
+    return LookupOutput()
+
+
+def extract_all_sizes(
+    town_districts: list[dict], terms: list[str], top_k_pages: int
+) -> Generator[AllLookupOutput, None, None]:
     for d in town_districts:
         town = d["Town"]
         districts = d["Districts"]
-        
+
         for district in districts:
             yield AllLookupOutput(
                 town=town,
                 district=district,
-                sizes={ term: extract_size(town, district, term, top_k_pages) for term in terms }
+                sizes={
+                    term: extract_size(town, district, term, top_k_pages)
+                    for term in terms
+                },
             )
+
 
 def main():
     districts_file = (
@@ -77,8 +105,13 @@ def main():
     )
 
     town_districts = load_jsonl(districts_file)
-    for result in extract_all_sizes(town_districts, ["min lot size", "min unit size"], 6):
-        print(result)
+    for result in extract_all_sizes(
+        town_districts, ["min lot size", "min unit size"], 6
+    ):
+        for term, v in result.sizes.items():
+            print(
+                f"{result.town} - {result.district.T}: {term} is {v.output.answer if v.output is not None else 'not found'}"
+            )
 
 
 if __name__ == "__main__":
--- zoning/utils.py
+++ zoning/utils.py
@@ -4,6 +4,11 @@ from pathlib import Path
 
 from git.repo import Repo
 
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+
 @cache
 def load_jsonl(path: Path) -> list:
     with path.open(encoding="utf-8") as f:
