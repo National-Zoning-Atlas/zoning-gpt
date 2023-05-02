f68c86bc55ba472022a4d811cda2ba38259942f0
Eesha Khanna <ek542@cornell.edu>
2023-05-02 18:20:16 -0400

    generalized eval.py to run for any numerical fields

--- zoning/data_processing/eval.py
+++ zoning/data_processing/eval.py
@@ -1,4 +1,5 @@
 import yaml
+import os
 
 from tqdm.auto import tqdm
 from tqdm.contrib.concurrent import thread_map
@@ -15,18 +16,18 @@ EVAL_METRICS_PATH = DATA_ROOT / "results" / "eval.yaml"
 EVAL_OUTPUT_PATH = DATA_ROOT / "results" / "eval.csv"
 
 
-def compute_eval_result(town: str, district_name: str, term: str, row):
+def compute_eval_result(town: str, district_name: str, term: str, term_code: str, row):
     outputs = extract_size(
         town,
         dict(T=district_name, Z=row.district_abb),
         term,
         6,
-        method=ExtractionMethod.MAP,
+        method=ExtractionMethod.NONE,
         # model_name="gpt-4"
         # model_name="gpt-3.5-turbo"
         model_name="text-davinci-003",
     )
-    gt_page = set(map(int, str(row.min_lot_size_page_gt).split(",")))
+    gt_page = set(map(int, str(row[f"{term_code}_page_gt"]).split(",")))
     for result in outputs:
         searched_pages = {r.page_number for r in result.search_pages}
         searched_pages_expanded = set(result.search_pages_expanded)
@@ -38,7 +39,7 @@ def compute_eval_result(town: str, district_name: str, term: str, row):
         yield {
             "town": town,
             "district": district_name,
-            "expected": row.min_lot_size_gt,
+            "expected": row[f"{term_code}_gt"],
             "actual": result.output.answer if result.output is not None else None,
             "correct_page_searched": any(gt_page & searched_pages_expanded),
             "correct_page_extracted": any(gt_page & extracted_pages),
@@ -48,71 +49,86 @@ def compute_eval_result(town: str, district_name: str, term: str, row):
             "extracted_pages": extracted_pages,
         }
 
+def append_to_yaml(file_path, term, data):
+    if not os.path.exists(file_path):
+        with open(file_path, "w", encoding="utf-8") as f:
+            yaml.dump({term: data}, f)
+    else:
+        with open(file_path, "r", encoding="utf-8") as f:
+            yaml_data = yaml.safe_load(f) or {}
+
+        yaml_data.update({term: data})
+
+        with open(file_path, "w", encoding="utf-8") as f:
+            yaml.dump(yaml_data, f)
 
 def main():
     gt = pd.read_csv(DATA_ROOT / "ground_truth.csv", index_col=["town", "district"])
 
-    # TODO: Run this evaluation on other fields besides min_lot_size as well
-    gt_min_lot = gt.query("~min_lot_size_gt.isna() & ~min_lot_size_page_gt.isna()")
-    term = "min lot size"
+    terms = ["min lot size", "min unit size"] # update to list of terms you want to run
+    
+    terms_code = [term.replace(" ", "_") for term in terms]
+    first = True
 
-    results = []
-    for result in thread_map(
-        lambda x: list(compute_eval_result(x[0][0], x[0][1], term, x[1])),
-        gt_min_lot.iterrows(),
-        total=len(gt_min_lot),
-    ):
-        results.extend(result)
+    for i, term in enumerate(terms):
+        gt_term = gt.query(f"~{terms_code[i]}_gt.isna() & ~{terms_code[i]}_page_gt.isna()")
 
-    results_df = pd.DataFrame(results)
+        results = []
+        for result in thread_map(
+            lambda x: list(compute_eval_result(x[0][0], x[0][1], term, terms_code[i], x[1])),
+            gt_term.iterrows(),
+            total=len(gt_term),
+        ):
+            results.extend(result)
 
-    # Attempt to normalize LLM responses
-    results_df = results_df.assign(
-        actual_normalized=results_df.actual.apply(clean_string_units)
-    ).explode("actual_normalized")
-    # Explode expected values so that we have one row per expected-actual-value pair.
-    results_df = results_df.assign(
-        expected_normalized=results_df.expected.apply(
-            lambda s: [float(f.strip()) for f in s.split(",")]
+        results_df = pd.DataFrame(results)
+
+        # Attempt to normalize LLM responses
+        results_df = results_df.assign(
+            actual_normalized=results_df.actual.apply(clean_string_units)
+        ).explode("actual_normalized")
+        # Explode expected values so that we have one row per expected-actual-value pair.
+        results_df = results_df.assign(
+            expected_normalized=results_df.expected.apply(
+                lambda s: [float(f.strip()) for f in s.split(",")]
+            )
+        ).explode("expected_normalized")
+        results_df = results_df.assign(
+            correct_answer=results_df.actual_normalized == results_df.expected_normalized
         )
-    ).explode("expected_normalized")
-    results_df = results_df.assign(
-        correct_answer=results_df.actual_normalized == results_df.expected_normalized
-    )
 
-    results_df.to_csv(EVAL_OUTPUT_PATH, index=False)
+        results_df.to_csv(EVAL_OUTPUT_PATH, index=False)
 
-    # groupby to calculate search page recall
-    search_results_df = (
-        results_df.groupby(by=["town", "district"])
-        .agg(
-            {
-                "correct_page_searched": "sum",
-                "correct_page_extracted": "sum",
-                "correct_answer": "sum",
-            }
+        # groupby to calculate search page recall
+        search_results_df = (
+            results_df.groupby(by=["town", "district"])
+            .agg(
+                {
+                    "correct_page_searched": "sum",
+                    "correct_page_extracted": "sum",
+                    "correct_answer": "sum",
+                }
+            )
+            .reset_index()
         )
-        .reset_index()
-    )
 
-    num_results = len(results_df)
-    num_correct_page_searched = float(
-        search_results_df["correct_page_searched"]
-        .apply(lambda x: 1 if x > 0 else 0)
-        .sum()
-    )
-    num_correct_page_extracted = float(
-        search_results_df["correct_page_extracted"]
-        .apply(lambda x: 1 if x > 0 else 0)
-        .sum()
-    )
-    num_correct_answer = float(
-        search_results_df["correct_answer"].apply(lambda x: 1 if x > 0 else 0).sum()
-    )
+        num_results = len(results_df)
+        num_correct_page_searched = float(
+            search_results_df["correct_page_searched"]
+            .apply(lambda x: 1 if x > 0 else 0)
+            .sum()
+        )
+        num_correct_page_extracted = float(
+            search_results_df["correct_page_extracted"]
+            .apply(lambda x: 1 if x > 0 else 0)
+            .sum()
+        )
+        num_correct_answer = float(
+            search_results_df["correct_answer"].apply(lambda x: 1 if x > 0 else 0).sum()
+        )
 
-    with EVAL_METRICS_PATH.open("w", encoding="utf-8") as f:
-        yaml.dump(
-            {
+        metrics = {
+            terms_code[i]: {
                 "num_results": num_results,
                 "num_correct_page_searched": num_correct_page_searched,
                 "num_correct_page_extracted": num_correct_page_extracted,
@@ -127,9 +143,10 @@ def main():
                 )
                 / num_correct_page_searched,
                 "answer_accuracy": num_correct_answer / len(search_results_df),
-            },
-            f,
-        )
+            }
+        }
+
+        append_to_yaml(EVAL_METRICS_PATH, term=terms_code[i], data=metrics)
 
 
 if __name__ == "__main__":
