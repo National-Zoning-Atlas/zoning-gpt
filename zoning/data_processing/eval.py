854c6db162ecfa2f784c29cb3fa5e475d733a3a8
Max Dumas <max@dumas.nyc>
2023-05-04 18:00:55 -0400

    Some code restructuring

--- zoning/data_processing/eval.py
+++ zoning/data_processing/eval.py
@@ -1,13 +1,12 @@
-import yaml
 import os
-
-from rich.progress import Progress
 from concurrent.futures import ThreadPoolExecutor
-from tqdm.contrib.concurrent import thread_map
+
 import pandas as pd
+import yaml
+from rich.progress import Progress
 
 from ..prompting.eval_results import clean_string_units
-from ..prompting.extract import extract_size, ExtractionMethod
+from ..prompting.extract import ExtractionMethod, extract_size
 from ..utils import get_project_root
 
 DATA_ROOT = get_project_root() / "data"
@@ -23,7 +22,7 @@ def compute_eval_result(town: str, district_name: str, term: str, term_code: str
         term,
         6,
         method=ExtractionMethod.MAP,
-        model_name="gpt-4"
+        model_name="gpt-4",
     )
     gt_page = set(map(int, str(row[f"{term_code}_page_gt"]).split(",")))
     for result in outputs:
@@ -52,18 +51,79 @@ def compute_eval_result(town: str, district_name: str, term: str, term_code: str
         }
 
 
-def append_to_yaml(file_path, term, data):
-    if not os.path.exists(file_path):
-        with open(file_path, "w", encoding="utf-8") as f:
-            yaml.dump({term: data}, f)
-    else:
-        with open(file_path, "r", encoding="utf-8") as f:
-            yaml_data = yaml.safe_load(f) or {}
+def evaluate_term(term: str, term_code: str, gt: pd.DataFrame, progress: Progress):
+    gt_term = gt.query(f"~{term_code}_gt.isna() & ~{term_code}_page_gt.isna()")
+    eval_task = progress.add_task(f"Evaluating {term}", total=len(gt_term))
 
-        yaml_data.update({term: data})
+    results = []
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        for result in executor.map(
+            lambda x: list(
+                compute_eval_result(x[0][0], x[0][1], term, term_code, x[1])
+            ),
+            gt_term.iterrows(),
+        ):
+            progress.advance(eval_task)
+            results.extend(result)
 
-        with open(file_path, "w", encoding="utf-8") as f:
-            yaml.dump(yaml_data, f)
+    results_df = pd.DataFrame(results)
+
+    # Attempt to normalize LLM responses
+    results_df = results_df.assign(
+        actual_normalized=results_df.actual.apply(clean_string_units)
+    ).explode("actual_normalized")
+    # Explode expected values so that we have one row per expected-actual-value pair.
+    results_df = results_df.assign(
+        expected_normalized=results_df.expected.apply(
+            lambda s: [float(f.strip()) for f in s.split(",")]
+        )
+    ).explode("expected_normalized")
+    results_df = results_df.assign(
+        correct_answer=results_df.actual_normalized == results_df.expected_normalized
+    )
+
+    # groupby to calculate search page recall
+    search_results_df = (
+        results_df.groupby(by=["town", "district"])
+        .agg(
+            {
+                "correct_page_searched": "sum",
+                "correct_page_extracted": "sum",
+                "correct_answer": "sum",
+            }
+        )
+        .reset_index()
+    )
+
+    num_results = len(results_df)
+    num_correct_page_searched = float(
+        search_results_df["correct_page_searched"]
+        .apply(lambda x: 1 if x > 0 else 0)
+        .sum()
+    )
+    num_correct_page_extracted = float(
+        search_results_df["correct_page_extracted"]
+        .apply(lambda x: 1 if x > 0 else 0)
+        .sum()
+    )
+    num_correct_answer = float(
+        search_results_df["correct_answer"].apply(lambda x: 1 if x > 0 else 0).sum()
+    )
+
+    return {
+        "num_results": num_results,
+        "num_correct_page_searched": num_correct_page_searched,
+        "num_correct_page_extracted": num_correct_page_extracted,
+        "num_correct_answer": num_correct_answer,
+        "page_search_recall": num_correct_page_searched / len(search_results_df),
+        "page_extract_recall": num_correct_page_extracted / len(search_results_df),
+        # This is the answer accuracy conditional on the correct page having been looked up by ES
+        "conditional_answer_accuracy": len(
+            search_results_df.query("correct_page_searched > 0 & correct_answer > 0")
+        )
+        / num_correct_page_searched,
+        "answer_accuracy": num_correct_answer / len(search_results_df),
+    }, results_df
 
 
 def main():
@@ -74,94 +134,20 @@ def main():
     terms_code = [term.replace(" ", "_") for term in terms]
     first = True
 
-    with Progress() as progress, ThreadPoolExecutor(max_workers=20) as executor:
+    metrics = {}
+
+    with Progress() as progress:
         term_task = progress.add_task("Terms", total=len(terms))
         for i, term in enumerate(terms):
-            gt_term = gt.query(
-                f"~{terms_code[i]}_gt.isna() & ~{terms_code[i]}_page_gt.isna()"
-            )
-            eval_task = progress.add_task(f"Evaluating {term}", total=len(gt_term))
-
-            results = []
-            for result in executor.map(
-                lambda x: list(
-                    compute_eval_result(x[0][0], x[0][1], term, terms_code[i], x[1])
-                ),
-                gt_term.iterrows(),
-            ):
-                progress.advance(eval_task)
-                results.extend(result)
-
-            results_df = pd.DataFrame(results)
-
-            # Attempt to normalize LLM responses
-            results_df = results_df.assign(
-                actual_normalized=results_df.actual.apply(clean_string_units)
-            ).explode("actual_normalized")
-            # Explode expected values so that we have one row per expected-actual-value pair.
-            results_df = results_df.assign(
-                expected_normalized=results_df.expected.apply(
-                    lambda s: [float(f.strip()) for f in s.split(",")]
-                )
-            ).explode("expected_normalized")
-            results_df = results_df.assign(
-                correct_answer=results_df.actual_normalized
-                == results_df.expected_normalized
-            )
-
+            metrics[term], results_df = evaluate_term(term, terms_code[i], gt, progress)
             results_df.to_csv(
                 EVAL_OUTPUT_PATH, index=False, mode="w" if first else "a", header=first
             )
-
-            # groupby to calculate search page recall
-            search_results_df = (
-                results_df.groupby(by=["town", "district"])
-                .agg(
-                    {
-                        "correct_page_searched": "sum",
-                        "correct_page_extracted": "sum",
-                        "correct_answer": "sum",
-                    }
-                )
-                .reset_index()
-            )
-
-            num_results = len(results_df)
-            num_correct_page_searched = float(
-                search_results_df["correct_page_searched"]
-                .apply(lambda x: 1 if x > 0 else 0)
-                .sum()
-            )
-            num_correct_page_extracted = float(
-                search_results_df["correct_page_extracted"]
-                .apply(lambda x: 1 if x > 0 else 0)
-                .sum()
-            )
-            num_correct_answer = float(
-                search_results_df["correct_answer"].apply(lambda x: 1 if x > 0 else 0).sum()
-            )
-
-            metrics = {
-                "num_results": num_results,
-                "num_correct_page_searched": num_correct_page_searched,
-                "num_correct_page_extracted": num_correct_page_extracted,
-                "num_correct_answer": num_correct_answer,
-                "page_search_recall": num_correct_page_searched / len(search_results_df),
-                "page_extract_recall": num_correct_page_extracted / len(search_results_df),
-                # This is the answer accuracy conditional on the correct page having been looked up by ES
-                "conditional_answer_accuracy": len(
-                    search_results_df.query(
-                        "correct_page_searched > 0 & correct_answer > 0"
-                    )
-                )
-                / num_correct_page_searched,
-                "answer_accuracy": num_correct_answer / len(search_results_df),
-            }
-
-            append_to_yaml(EVAL_METRICS_PATH, term=terms_code[i], data=metrics)
-
             progress.advance(term_task)
             first = False
+
+    with EVAL_METRICS_PATH.open("w", encoding="utf-8") as f:
+        yaml.dump(metrics, f)
 
 
 if __name__ == "__main__":
