f0f8d0879b007065ce6a6b8763b811c65c0e6224
Jan Carbonell <jcllobet@gmail.com>
2023-10-11 00:39:22 -0400

    added configurations to debug and imported relevant env vars on init

---
+++ zoning/__init__.py
@@ -0,0 +1,24 @@
+"""
+Initialization for the OpenAI configuration module.
+
+This module ensures the OpenAI key is loaded at the start of the application.
+"""
+
+import os
+import openai
+from dotenv import load_dotenv
+
+# Get the absolute path to the .env file
+dotenv_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), ".env")
+
+# Load environment variables from .env
+load_dotenv(dotenv_path)
+
+# Check and set the OpenAI API key
+open_api_key = os.environ.get("OPENAI_API_KEY")
+if not open_api_key:
+    raise ValueError("OPEN_API_KEY not found in .env file!")
+openai.api_key = open_api_key
+
+# to validate key is loaded, print the first two letters
+print(openai.api_key[:2])
