bb92e602e91e3f4ca9b48b05c552f8ec23cfdb91
Nathaniel-Nirmal <na453@cornell.edu>
2024-02-29 07:23:48 -0500

    Minor Changes for new term

--- commands.sh
+++ commands.sh
@@ -1,1 +1,1 @@
-python -m zoning.data_processing.eval --num-eval-rows 30 --terms min_unit_size --search-method experiment_3 --extraction-method answer_confirm --k 10
+python -m zoning.data_processing.eval --num-eval-rows 30 --terms min_parking_spaces --search-method experiment_3 --extraction-method tournament_reduce --k 10
