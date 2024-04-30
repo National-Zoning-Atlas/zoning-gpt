3f5ce40919a9afae2584ceb9d39787c42cf7de76
Yuyang Wang <yw2545@cornell.edu>
2024-04-29 22:05:04 -0400

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -17,3 +17,13 @@ python -m zoning.data_processing.eval --num-eval-rows 30 --terms max_lot_coverag
 python -m zoning.data_processing.eval --num-eval-rows 30 --terms min_lot_size,min_unit_size,max_height,min_parking_spaces,max_lot_coverage --search-method experiment_3 --extraction-method answer_confirm --k 10
 
 python -m zoning.data_processing.eval --num-eval-rows 30 --terms "floor_to_area_ratio" --search-method experiment_3 --extraction-method answer_confirm --k 10
+
+python zoning/main/search.py | python zoning/main/extract.py | python zoning/main/table_printer.py
+python zoning/main/search.py | python zoning/main/extract.py | python zoning/main/json_printer.py
+
+
+python zoning/main/search.py --term "min lot size" --town "andover" --district_name "Andover Lake" --district_abb "AL" --search_method "experiment_3" --k 10 | python zoning/main/extract.py --extraction_method "answer_confirm" --model_name "gpt-4-1106-preview" --tournament_k 10 | python zoning/main/table_printer.py
+
+pdm run python -m zoning.main.search | pdm run python -m zoning.main.extract | pdm run python -m zoning.main.table_printer
+
+pdm run python -m zoning.main.search --term "min lot size" --town "andover" --district_name "Andover Lake" --district_abb "AL" --search_method "experiment_3" --k 10
